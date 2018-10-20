a24d752427319b06f57f6288b165e2cd9c94a0cf
Roman Varkuta <roman.varkuta@gmail.com>
2018-10-20 21:54:27 +0300

    Add tests for exceptions (#8)

---
+++ tests/Exception/InvalidElementTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace kartavik\Collections\Tests\Exception;
+
+use PHPUnit\Framework\TestCase;
+use kartavik\Collections\Exception\InvalidElement;
+
+/**
+ * Class InvalidElementTest
+ * @package kartavik\Collections\Tests\Exception
+ * @coversDefaultClass InvalidElement
+ * @internal
+ */
+class InvalidElementTest extends TestCase
+{
+    /** @var InvalidElement */
+    protected $fakeInvalidElement;
+
+    protected function setUp(): void
+    {
+        $this->fakeInvalidElement = new InvalidElement(
+            new \stdClass(),
+            \Exception::class
+        );
+    }
+
+    public function testGetVar(): void
+    {
+        $this->assertEquals(new \stdClass(), $this->fakeInvalidElement->getVar());
+    }
+
+    public function testGetNeedType(): void
+    {
+        $this->assertEquals(\Exception::class, $this->fakeInvalidElement->getNeedType());
+    }
+}
---
+++ tests/Exception/UnprocessedTypeTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace kartavik\Collections\Tests\Exception;
+
+use kartavik\Collections\Exception\UnprocessedType;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class UnprocessedTypeTest
+ * @package kartavik\Collections\Tests\Exception
+ * @coversDefaultClass UnprocessedType
+ * @internal
+ */
+class UnprocessedTypeTest extends TestCase
+{
+    /** @var UnprocessedType */
+    protected $fakeUnprocessedType;
+
+    protected function setUp(): void
+    {
+        $this->fakeUnprocessedType = new UnprocessedType(\stdClass::class);
+    }
+
+    public function testGetType(): void
+    {
+        $this->assertEquals(\stdClass::class, $this->fakeUnprocessedType->getType());
+    }
+}
